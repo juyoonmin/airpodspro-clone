08b6b03ca16044209c2a97225e8fb5f1a5ed1e13
juyoonmin <goaxhfl0109@naver.com>
2023-08-03 21:51:06 +0900

    scroll-section-3 캔버스 스크롤위치에 따른 포지션 변경, scroll-section-2에서 미리 그려주기 설정

--- js/main.js
+++ js/main.js
@@ -112,7 +112,7 @@
             values: {
                 rect1X: [0, 0, { start: 0, end: 0 }],
                 rect2X: [0, 0, { start: 0, end: 0 }],
-                rectStartY: 0, // 캔버스 사각형 기준점
+                rectStartY: 0 // 캔버스 사각형 기준점
             }
         }
     ];
@@ -308,9 +308,55 @@
                     objs.pinC.style.transform = `scaleY(${calcValues(values.pinC_scaleY, currentYoffset)})`;
                 }
 
+                // currentScene 3에서 쓰는 캔버스를 미리 그려주기 시작
+                if (scrollRatio > 0.9) {
+                    const objs = sceneInfo[3].objs;
+                    const values = sceneInfo[3].values;
+                    const widthRatio = window.innerWidth / objs.canvas.width;
+                    const heightRatio = window.innerHeight / objs.canvas.height;
+                    let canvasScaleRatio;
+
+                    if (widthRatio <= heightRatio) {
+                        // 캔버스보다 브라우저 창이 홀쭉한 경우
+                        canvasScaleRatio = heightRatio;
+                    } else {
+                        // 캔버스보다 브라우저 창이 납작한 경우
+                        canvasScaleRatio = widthRatio;
+                    }
+
+                    objs.canvas.style.transform = `scale(${canvasScaleRatio})`;
+                    objs.context.fillStyle = 'white';
+                    objs.context.drawImage(objs.images[0], 0, 0);
+
+                    // 캔버스 사이즈에 맞춰 가정한 innerWidth와 innerHeight
+                    const recalculatedInnerWidth = document.body.offsetWidth / canvasScaleRatio;
+                    const recalculatedInnerHeight = window.innerHeight / canvasScaleRatio;
+
+                    const whiteRectWidth = recalculatedInnerWidth * 0.15;
+                    values.rect1X[0] = (objs.canvas.width - recalculatedInnerWidth) / 2;
+                    values.rect1X[1] = values.rect1X[0] - whiteRectWidth;
+                    values.rect2X[0] = values.rect1X[0] + recalculatedInnerWidth - whiteRectWidth;
+                    values.rect2X[1] = values.rect2X[0] + whiteRectWidth;
+
+                    // 좌우 흰색 박스 그리기
+                    objs.context.fillRect(
+                        parseInt(values.rect1X[0]),
+                        0,
+                        parseInt(whiteRectWidth),
+                        objs.canvas.height
+                    );
+                    objs.context.fillRect(
+                        parseInt(values.rect2X[0]),
+                        0,
+                        parseInt(whiteRectWidth),
+                        objs.canvas.height
+                    );
+                }
+
                 break;
             case 3:
                 // console.log('3 play');
+                let step = 0;
                 // 가로/세로 모두 꽉 차게 하기 위해 여기서 세팅(계산 필요)
                 const widthRatio = window.innerWidth / objs.canvas.width; //캔버스의 가로비율
                 const heightRatio = window.innerHeight / objs.canvas.height; //캔버스의 세로비율
@@ -325,16 +371,20 @@
                 }
 
                 objs.canvas.style.transform = `scale(${canvasScaleRatio})`; //캔버스의 크기를 브라우저 창에 맞게 조정
+                objs.context.fillStyle = 'white'; //캔버스의 배경색을 흰색으로 설정
                 objs.context.drawImage(objs.images[0], 0, 0);
 
                 // 캔버스 사이즈에 맞춰 가정한 innerWidth와 innerHeight
                 const recalculatedInnerWidth = document.body.offsetWidth / canvasScaleRatio; //캔버스의 가로비율에 맞춰서 innerWidth를 조정
-                const recalculatedInnerHeight = document.body.offsetWidth / canvasScaleRatio; //캔버스의 세로비율에 맞춰서 innerHeight를 조정
+                const recalculatedInnerHeight = window.innerHeight / canvasScaleRatio; //캔버스의 세로비율에 맞춰서 innerHeight를 조정
 
                 if (!values.rectStartY) { //캔버스의 시작점을 고정시키기 위해 
-                    values.rectStartY = objs.canvas.getBoundingClientRect().top; //캔버스의 top 위치를 계산
-                    values.rect1X[2].end = values.rectStartY / scrollHeight; //첫번째 직사각형의 시작점을 계산
-                    values.rect2X[2].end = values.rectStartY / scrollHeight; //두번째 직사각형의 시작점을 계산
+                    values.rectStartY = objs.canvas.getBoundingClientRect().top;
+
+                    values.rect1X[2].start = (window.innerHeight / 2) / scrollHeight; //캔버스의 높이 및 스케일링 비율에 따라서 직사각형의 시작 y 위치를 계산
+                    values.rect2X[2].start = (window.innerHeight / 2) / scrollHeight;
+                    values.rect1X[2].end = values.rectStartY / scrollHeight;
+                    values.rect2X[2].end = values.rectStartY / scrollHeight;
                 }
 
                 const whiteRectWidth = recalculatedInnerWidth * 0.15; // innerWidth를 기준으로 흰색 직사각형의 너비를 계산
@@ -348,13 +398,25 @@
                     parseInt(calcValues(values.rect1X, currentYoffset)), //첫번째 직사각형의 x 위치를 계산
                     0, //첫번째 직사각형의 y 위치를 계산
                     parseInt(whiteRectWidth), //첫번째 직사각형의 너비를 계산
-                    objs.canvas.height); //첫번째 직사각형의 높이를 계산
+                    objs.canvas.height
+                ); //첫번째 직사각형의 높이를 계산
                 objs.context.fillRect(parseInt(calcValues(values.rect2X, currentYoffset)),
                     0,
                     parseInt(whiteRectWidth),
-                    objs.canvas.height); 
+                    objs.canvas.height
+                );
 
-                    
+                if (scrollRatio < values.rect1X[2].end) { // 스크롤이 직사각형(캔버스)의 시작점에 도달하기 전까지는 직사각형이 고정되어 있음
+                    step = 1;
+                    objs.canvas.classList.remove('sticky-canvas');
+                } else { // 스크롤이 직사각형(캔버스)의 시작점에 도달하면 직사각형이 스크롤에 따라서 움직임
+                    step = 2;
+                    // 이미지 블렌드
+                    objs.canvas.classList.add('sticky-canvas'); //캔버스를 fixed로 고정
+                    objs.canvas.style.top = `${-(objs.canvas.height - objs.canvas.height * canvasScaleRatio) / 2}px`; //캔버스의 위치를 조정
+                }
+
+
                 break;
         }
 
